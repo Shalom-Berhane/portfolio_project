975a5ec48369078a88301a79d2e4bb4879c192ec
Shalom <shalombr16@gmail.com>
2020-05-05 11:12:21 +0300

    Done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,10 @@ class Blog(models.Model):
     pub_data = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    def pub_date_pretty(self):
+        return self.pub_data.strftime('%b %e %Y')
+    def summary(self):
+        return self.body[:100]
